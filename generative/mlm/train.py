fbd2e2b09f373d90f33a2127cfebca9d5c82f95b
matthew <meynm@oregonstate.edu>
2019-03-24 11:51:59 -0700

    acoustic model script

--- generative/mlm/train.py
+++ generative/mlm/train.py
@@ -260,7 +260,7 @@ def main(opts):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_dir", default="../../clean_preproc")
+    parser.add_argument("--data_dir", default="data/fugue_preproc")
     parser.add_argument("--max_epochs", type=int, default=5000)
     parser.add_argument("--max_w", type=int, default=4096)
     parser.add_argument("--batch_size", type=int, default=8)
